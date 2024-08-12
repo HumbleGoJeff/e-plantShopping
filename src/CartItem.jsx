b70d45f2b3fbce7fe6fc1e4f0f47c6aa2caaeabd
HumbleGoJeff <3181465381gjf@gmail.com>
2024-08-12 02:57:34 -0400

    change

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,43 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalPrice = 0;
+    items.forEach((item) => {
+      totalPrice += parseFloat(item.cost.replace("$", "")) * item.quantity;
+    });
+    return totalPrice;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ ...item, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ ...item, quantity: item.quantity - 1 }));
+    } else if (item.quantity === 1) {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    const nameToRemove = item.name;
+    dispatch(removeItem(nameToRemove));
+    setAddedToCart(({ [nameToRemove]: _, ...rest }) => rest);
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const { cost, quantity } = item;
+    return parseFloat(cost.replace("$", "")) * quantity;
   };
 
   return (
